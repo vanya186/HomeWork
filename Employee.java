83ea653a5f52f9fb6b7f91ec3b15dd54d8449d12
Vanya87 <90348914+vanya186@users.noreply.github.com>
2022-04-11 21:46:10 +0300

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,34 @@
+public class Employee {
+    private String full_name;
+    private String position;
+    private String email;
+    private int telephone;
+    private int salary;
+    private int age;
+
+    public Employee(String full_name, String position, String email, int telephone, int salary, int age) {
+        this.full_name = full_name;
+        this.position = position;
+        this.email = email;
+        this.telephone = telephone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "full_name ='" + full_name + '\'' +
+                ", position ='" + position + '\'' +
+                ", email ='" + email + '\'' +
+                ", telephone = " + telephone +
+                ", salary = " + salary +
+                ", age = " + age +
+                '}';
+    }
+}
+
