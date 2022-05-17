cd72700b934e51ac5ae6c86ee266349e95e656a6
Vanya87 <90348914+vanya186@users.noreply.github.com>
2022-05-17 20:51:56 +0300

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,24 @@
+package Lesson1;
+
+public class Course {
+
+    Obstacle[] obstacles;
+
+    public Course(Obstacle[] obstacles){
+        this.obstacles = obstacles;
+    }
+
+    public void doIt(Team team){
+        Competitor[] teamMembers = team.getMembers();
+        if(teamMembers.length > 0){
+            for (Competitor c: teamMembers){
+                for (Obstacle o: obstacles){
+                    o.doIt(c);
+                    if (!c.isOnDistance()) break;
+                }
+            }
+        } else {
+            System.out.println("There are no members in the team!");
+        }
+    }
+}
