c53512eaf2051be56eb54864ebe85a2836e34b73
Vanya87 <90348914+vanya186@users.noreply.github.com>
2022-05-17 21:25:43 +0300

    Delete Competitor.java

--- Competitor.java
+++
@@ -1,9 +0,0 @@
-package Lesson1;
-
-public interface Competitor {
-    void run(int distance);
-    void swim(int distance);
-    void jump(int height);
-    boolean isOnDistance();
-    void showResult();
-}
